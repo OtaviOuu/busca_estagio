6cb343c8d62d645597956f4c57a8b0a54013394a
otaviouu <otavio.venancio.oliveira@usp.br>
2025-12-20 21:51:23 -0300

    feat: update Estagio details rendering to use dynamic data from the database

--- lib/busca_estagio_web/live/estagio_live/show.ex
+++ lib/busca_estagio_web/live/estagio_live/show.ex
@@ -12,15 +12,9 @@ defmodule BuscaEstagioWeb.EstagioLive.Show do
       <.header>
         Estágio Details
       </.header>
-      <div class="mx-4 my-8 card w-full bg-base-100 p-4 shadow-xl">
-        <h2 class="text-2xl font-bold mb-4">Estágio Back-office</h2>
-        <p><strong>Universidade:</strong> USP-ICMC</p>
-        <p>
-          <strong>Descrição:</strong>
-          Este estágio envolve atividades de suporte administrativo e operacional no setor de back-office.
-        </p>
-        <p><strong>Data de Captura:</strong> {Date.utc_today()}</p>
-      </div>
+      <h2>{@estagio.titulo}</h2>
+      <a class="text-blue-500 underline" href={@estagio.link}>Link da postageem</a>
+      {raw(@estagio.descricao)}
     </Layouts.app>
     """
   end
